9b0802895e3732605e86d9a872919cd792be23b8
Madhuri8459 <131444689+Madhuri8459@users.noreply.github.com>
2023-06-12 14:47:30 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,89 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+    
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+        <button class="btn btn-outline-dark text-white my-2 my-sm-0" type="submit">Get free delivery on orders over $100</button>
+  
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#"> <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"></a>
+      </li>
+      
+      
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+        <button class="btn btn-outline-dark text-white my-2 my-sm-0" type="submit" id='b1' >Create an account</button>
+      <button class="btn btn-outline-dark text-white my-2 my-sm-0" type="submit" id='b2' >Sign in</button>
+    </form>
+  </div>
+</nav>
+
+<nav>
+  <div>
+      <img class="image" src="tag.jpg" />
+  </div>
+  <div class="logo">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="cart4" viewBox="0 0 16 16">
+      <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
+    </svg>
+     
+     <h3 id="p23">I Novo-Shop</h3>
+     
+     
+  </div>
+  <ul class="menu">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="person" viewBox="0 0 16 16">
+          <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"/>
+        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="cart2" viewBox="0 0 16 16">
+          <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
+        </svg>
+        <div class="para">
+          <h4>0</h4>
+        </div>
+    
+    <li><a id="p24" href="#">Clothing..</a></li>
+    <li><a id="p25" href="#">Men</a></li>
+    <li><a id="p26" href="#">Women</a></li>
+  </ul>
+</nav>
+<br/>
+<br/>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link active" aria-current="page" href="#"></a>
+          <a id="p11" class="nav-link" href="#">  Download the app      </a>
+          <h4 id="p8">      Get an exclusive $5 off code </h4>
+          <a id="p12" class="nav-link" href="#">Return when you are ready</a>
+          <br/>
+          <h4 id="p9">60 days of free returns</h4>
+          <a id="p13" class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">signup for our newsletter</a>
+          <h4 id="p40">50% off your first order</h4>
+        </div>
+      </div>
+    </div>
+    </nav>
+    
+    </>
+  )
+}
+
+export default Navbar
---
+++ src/components/product.js
@@ -0,0 +1,67 @@
+import React from 'react'
+
+function product() {
+  return (
+    <>
+   <b><h1 id="p14">Shop with  
+   <br/> confidence</h1> </b>
+  <img id="p15" src="WhatsApp Image 2023-06-06 at 10.15.19 AM.jpeg"/>
+  <h1 id="p16">Get 25% off during our one-time sale </h1>
+  <p id="p17">Most of the products are limited releases that won't come back</p>
+  <p id="p18">Get your favorite items while they're in stock</p>
+  <center><button type="button" id="b101" class="btn btn-dark">Get access to our one-time sale</button></center>
+
+   <p id="p19">Shop by Category</p>
+   <img id="m1" src="https://t4.ftcdn.net/jpg/05/32/38/33/360_F_532383385_huEKHwbwEqngNLw5WHfayoRlnouUDMeH.jpg"/>
+   <h3 id="I101" >MEN(3)</h3>
+   <img id="m2" src="https://t4.ftcdn.net/jpg/02/12/36/31/360_F_212363162_mfFdAKSebSGzPe9jRFcPLJmd89yIj5cM.webp"/>
+   <h2 id="I102">WOMEN(6)</h2>
+   <img id="m3" src="https://yt3.googleusercontent.com/kuG0t4b_bTsNnGHw-FQva380BdNJJylaMeMuXklJggO45T-mKajSCjNv3t2W0CYL-7xEz5N1AJA=s900-c-k-c0x00ffffff-no-rj"/>
+   <h2 id="I103">ALL(0)</h2>
+ <img id="m4" src="https://static.vecteezy.com/system/resources/previews/006/986/815/large_2x/hand-hold-light-bulb-on-black-background-inspiration-and-creativity-concepts-free-photo.jpg"/>
+  <h2 id="I104">KIDS(1)</h2>
+ <img id="m5" src="https://static.vecteezy.com/system/resources/previews/006/986/815/large_2x/hand-hold-light-bulb-on-black-background-inspiration-and-creativity-concepts-free-photo.jpg"/>
+ <h2 id="I105">ACESSORIES(0)</h2>
+
+   <img id="m6" src="https://b-edge.friartux.com/f00000000189637/www.friartux.com/pub/media/magefan_blog/titlepage2.jpg"/>
+  <p id="p20">Browse all categories -</p>
+   <p id="p21">Trending Products</p>
+   <img id="m6" src="https://i8.amplience.net/i/jpl/sz_570599_b?qlt=92&w=750&h=580&v=1&fmt=auto"/>
+   <p id="n1" > adidas</p>
+   <h3 id="n2" >$22.00</h3>
+   <p id="n3">ghghhghghjj</p>
+   <img id="m7" src="https://b-edge.friartux.com/f00000000189637/www.friartux.com/pub/media/magefan_blog/titlepage2.jpg"/>
+   <p id="n4" >Men suit 2 </p>
+   <h3 id="n5" >$140.00</h3>
+   <p id="n6">ntray to popular belief ,loream ipsum is not<br/> simply random text.it has roots in a piece of<br/> classical latin literaturefrom 45 BC, making <br/> it over 2000 years old Richard McClintock,a <br/> Latin professor at Hampden Sydeny College<br/> in Virginia, looking up one of the more <br/> latin wordconsectuar</p>
+   <img id="m8" src="https://static3.bigstockphoto.com/5/0/2/large1500/205439965.jpg"/>
+   <p id="n7" >Nike Airforce </p>
+   <h3 id="n8" >$2000.00</h3>
+   <p id="n9">ntray to popular belief ,loream ipsum is not<br/> simply random text.it has roots in a piece of<br/> classical latin literaturefrom 45 BC, making <br/> it over 2000 years old Richard McClintock,a <br/> Latin professor at Hampden Sydeny College<br/> in Virginia, looking up one of the more <br/> latin wordconsectuar</p>
+   <img id="m9" src="https://static3.bigstockphoto.com/5/0/2/large1500/205439965.jpg"/>
+   <p id="n10" >Nike Airforce </p>
+   <h3 id="n11" >$2000.00</h3>
+   <p id="n12">ntray to popular belief ,loream ipsum is not<br/> simply random text.it has roots in a piece of<br/> classical latin literaturefrom 45 BC, making <br/> it over 2000 years old Richard McClintock,a <br/> Latin professor at Hampden Sydeny College<br/> in Virginia, looking up one of the more <br/> latin wordconsectuar</p>
+  <p id="p22">Shop the collection -</p>
+  
+  <img src="t-shirt.png" id="b16" width="60px" height="70px"/>
+  <p id="b8"> Free returns</p>
+  <p id="b9"> Not what you expected? Place it back in the<br/> parcel and attach the pre-paid postage<br/>stamp</p>
+
+<img src="calender.jpeg" id="b17" width="60px" height="70px"/>
+  <p id="b10">Same day delivery</p>
+  <p id="b11">We offer a delivery service that has never<br/>been done before.Checkout today and <br/>receive your products within hours.</p>
+
+  <img src="gift - Copy.jpeg" id="b18" width="50px" height="60px"/>
+<p id="b12">All year discount</p>
+<p id="b13">Looking for a deal?You can use the code<br/>"ALLYEAR" at checkout and get money off<br/> all year round </p>
+
+<img src="planet.jpeg" id="b19" width="100px" height="70px"/>
+<p id="b14">For the planet</p>
+<p id="b15">we've pledge 1% of sales to the<br/> preservation of the natural<br/> environment</p>
+  
+    </>
+  )
+}
+
+export default product
